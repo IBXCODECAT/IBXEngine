3bb92e498a76b8fc242a0823612a739ff2c9d5f8
IBXCODECAT <nathan.schmitt7@gmail.com>
2024-06-12 20:03:27 -0500

    Create Game Window

---
+++ GameInstance.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningOpenTK
+{
+    internal class GameInstance : GameWindow
+    {
+        public GameInstance(int width, int height, string title) : base(new GameWindowSettings(), new NativeWindowSettings()
+        {
+            Size = new OpenTK.Mathematics.Vector2i(width, height),
+            Title = title
+        }) { }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if(KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (GameInstance game = new(800, 600, "Learning OpenTK"))
+            {
+                game.Run();
+            }
         }
     }
 }
